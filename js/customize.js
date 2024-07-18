5ec2e48fa3da052879e8344bb3ba6a8dedc72c8e
anniewuu00 <annie@hyweb.com.tw>
2024-07-18 12:07:40 +0800

    20240718 上傳資料

---
+++ js/customize.js
@@ -0,0 +1,273 @@
+// 自行加入的JS請寫在這裡
+$(function () {
+  // 首頁輪播
+  $('.mpSlider').slick({
+    mobileFirst: true,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    fade: true,
+    lazyLoaded: true,
+    lazyLoad: 'ondemand',
+    ease: 'ease',
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    customPaging: function (slider, i) {
+      var title = $(slider.$slides[i]).find('img').attr('alt').trim();
+      return $('<button type="button" aria-label="' + title + '"/>').text(title);
+    },
+  });
+  // 問題選粹
+  $('.question_selection_group').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  // 廣告輪播
+  $('.adSlider').slick({
+    mobileFirst: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrow: true,
+    lazyLoaded: true,
+    lazyLoad: 'ondemand',
+    ease: 'ease',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+        },
+      },
+    ],
+  });
+  //燈箱slick+lightBox組合
+  $('.cp_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    focusOnSelect: true,
+    accessibility: true,
+    lazyLoad: 'ondemand',
+    ease: 'ease',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 545,
+        settings: {
+          arrows: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
+  //
+  $('.cppic_slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    // pauseOnHover: true,
+    // pauseOnFocus: true,
+    // focusOnSelect: true,
+    // accessibility: true,
+    // lazyLoad: 'ondemand',
+    // ease: 'ease',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 545,
+        settings: {
+          arrows: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
+  // cp_photo
+  $('.Slider-for').on('init reInit afterChange', function (event, slick, currentSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $('.controls').html(i + '/' + slick.slideCount);
+  });
+  $('.Slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    swipe: false,
+    swipeToSlide: false,
+    lazyLoad: 'ondemand',
+    asNavFor: '.Slider-nav',
+    infinite: true,
+  });
+  $('.Slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.Slider-for',
+    dots: true,
+    arrows: true,
+    lazyLoad: 'ondemand',
+    focusOnSelect: true,
+    infinite: true,
+  });
+
+  // password_toggle
+  var passShow = false;
+  $('.password_toggle').each(function (index, el) {
+    $(this)
+      .find('.btn-icon')
+      .off()
+      .click(function (e) {
+        if (!passShow) {
+          $(this).children('i').removeClass().addClass('i_show');
+          $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
+          passShow = true;
+          // console.log(passShow);
+        } else {
+          $(this).children('i').removeClass().addClass('i_hide');
+          $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
+          passShow = false;
+          // console.log(passShow);
+        }
+        e.preventDefault();
+      });
+  });
+  //sticky sidebar
+  // if ($('.stickySidebar').length > 0) {
+  //   var stickySidebar = new StickySidebar('.stickySidebar', {
+  //     containerSelector: '.main',
+  //     topSpacing: 93,
+  //     bottomSpacing: 0,
+  //     minWidth: 768,
+  //     resizeSensor: true,
+  //   });
+  // }
+
+  // 熱門問題
+
+  $('.question_list .question_title').on('click', function () {
+    if ($(this).hasClass('active')) {
+      $(this).siblings('.answer_content').stop().slideUp();
+      $(this).removeClass('active');
+    } else {
+      $(this).siblings('.answer_content').stop().slideDown();
+      $(this).addClass('active');
+    }
+  });
+
+  // 加１
+  $('.answer_content .solved_or_not ')
+    .find('.helpful')
+    .click(function () {
+      $(this).parents('ul').siblings('.add1').addClass('appear');
+      setTimeout(function () {
+        $('.add1').removeClass('appear');
+      }, 1500);
+    });
+
+  // 燈箱
+
+  Fancybox.bind('[data-fancybox]', {
+    // Custom options for all galleries
+  });
+});
+
+// tab功能
+tabFunction({
+  target: '.tabSet',
+  openFirst: false, // 預設先展開所有內容，鍵盤的自動開合功能無效
+  openSwitch: true, // 是否可開合/切換
+  autoClose: true, // 自動關閉其他開啟內容
+  modeSwitch: false, // 預設模式自動切換，尺寸以上tab功能，尺寸以下手風琴功能
+  width: 767, // 尺寸以上tab功能，尺寸以下手風琴功能
+  index: 0, // 預設開啟第幾個
+});
